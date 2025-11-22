cf292ae98868b056bf900ef4d7f2977136a31de5
KayRielz <azrilpribadi135@gmail.com>
2025-11-22 18:05:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,33 @@
 // HERO SLIDER — FINAL FIX VERSION
 // ===============================
 
-// Ambil semua slide
 let slides = document.querySelectorAll(".slide");
 let index = 0;
 
-// Fungsi untuk menampilkan slide
 function showSlide(i) {
     slides.forEach(s => s.classList.remove("active"));
     slides[i].classList.add("active");
 }
 
-// Auto next slide
 function nextSlide() {
     index = (index + 1) % slides.length;
     showSlide(index);
 }
 
-// Jalan setiap 6 detik
 setInterval(nextSlide, 6000);
-
-// Tampilkan awal
 showSlide(index);
-    // toggle icon
+
+
+
+// =======================
+// MOBILE NAV TOGGLE
+// =======================
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+
     if (navLinks.classList.contains('show')) {
         menuToggle.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     } else {
